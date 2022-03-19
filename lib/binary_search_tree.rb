35a8395700d2826b1315d06712a4659083494e85
Ziyan Junaideen <ziyan@jdeen.com>
2022-03-19 13:45:02 +0530

    Init

---
+++ lib/binary_search_tree.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "binary_search_tree/version"
+
+module BinarySearchTree
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/binary_search_tree/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BinarySearchTree
+  VERSION = "0.1.0"
+end
